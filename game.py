7cb166c1222d7d9a9c6b53a0140444997d1ef368
FHegardt <39218412+FHegardt@users.noreply.github.com>
2022-04-02 12:38:29 +0200

    upd params again

--- game.py
+++ game.py
@@ -84,13 +84,13 @@ class SnakeGameAI:
         game_over = False
         if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
-            reward = -10
+            reward = -12
             return reward, game_over, self.score
             
         # 4. place new food or just move
         if self.head == self.food:
             self.score += 1
-            reward = 15
+            reward = 8
             self._place_food()
         else:
             self.snake.pop()
