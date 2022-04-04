19d25a480b8afb4644aacfee373f49615f627e2f
FHegardt <39218412+FHegardt@users.noreply.github.com>
2022-04-04 11:42:15 +0200

    testing snake in web-browser

--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html", test = agent.train())
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -17,3 +17,7 @@ def logout():
 @auth.route('/sign-up')
 def sign_up():
     return render_template("sign_up.html")
+
+@auth.route('/snake')
+def snake():
+    return render_template("reinforcement_snake.html", test = agent.train())
