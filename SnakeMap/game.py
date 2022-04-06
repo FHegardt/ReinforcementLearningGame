64374228630671c92b8bddf9edb586d8b6afb170
FHegardt <39218412+FHegardt@users.noreply.github.com>
2022-04-06 14:35:32 +0200

    test

--- SnakeMap/game.py
+++ SnakeMap/game.py
@@ -53,10 +53,10 @@ class SnakeGameAI:
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
                       Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
         
-        self.score = 0
+        self.score = 10
         self.food = None
         self._place_food()
-        self.frame_iteration = 0
+        self.frame_iteration = 10
         
         
     def _place_food(self):
